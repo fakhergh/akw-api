a9b953d5976fd808ce733dac3139375d9237e211
Fakher Ghouili <fakherghouili@gmail.com>
2025-01-10 18:12:25 +0100

    Create jwt util

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,4 +6,14 @@ export const Config = {
         name: process.env.DATABASE_NAME!,
         uri: process.env.DATABASE_URI!,
     },
+    jwt: {
+        access: {
+            secret: process.env.ACCESS_TOKEN_SECRET ?? 'secure-access',
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN ?? '15m',
+        },
+        refresh: {
+            secret: process.env.REFRESH_TOKEN_SECRET ?? 'secure-refresh',
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN ?? '1d',
+        },
+    },
 };
---
+++ src/utils/jwt.util.ts
@@ -0,0 +1,34 @@
+import { JwtPayload, sign, verify } from 'jsonwebtoken';
+
+import { Config } from '@/config';
+import { RoleType } from '@/types/role.type';
+
+const accessTokenCredentials = Config.jwt.access;
+const refreshTokenCredentials = Config.jwt.refresh;
+
+export interface Payload extends JwtPayload {
+    _id: string;
+    role: RoleType;
+}
+
+export class JsonWebToken {
+    static signJwt(payload: string | object): string {
+        return sign(payload, accessTokenCredentials.secret, {
+            expiresIn: accessTokenCredentials.expiresIn,
+        });
+    }
+
+    static verifyJwt(token: string): Payload {
+        return verify(token, accessTokenCredentials.secret) as Payload;
+    }
+
+    static signJwtRefresh(payload: string | object): string {
+        return sign(payload, refreshTokenCredentials.secret, {
+            expiresIn: refreshTokenCredentials.expiresIn,
+        });
+    }
+
+    static verifyJwtRefresh(token: string): Payload {
+        return verify(token, refreshTokenCredentials.secret) as Payload;
+    }
+}
